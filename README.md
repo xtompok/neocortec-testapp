8ea091b1485185c330de3dde0c2c06e99a3e7efd
Tomas Jethro Pokorny <xtompok@gmail.com>
2016-06-16 17:33:36 +0200

    Simple sending app.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # neocortec-testapp
 Some demo apps for Neocortec wireless modules
+
+
+Connection
+==========
+Demoboard has two serial ports connected to FTDI USB-RS232 bridge. In my Linux, an application serial port is the first, e.g. /dev/ttyUSB0 and the system serial port is the second, e.g. /dev/ttyUSB1.
---
+++ sendmsg.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import serial
+import sys
+import time
+
+def sendpkt(target,data,port=0):
+    pkt = [0x03,len(data)+3,(target>>8)&0xFF,target&0xFF,port]+data
+    pkt = bytearray(pkt)
+    print("Waiting for CTS")
+    while (not ser.getCTS()):
+        time.sleep(0.0001)
+    print("Got CTS")
+    ser.write(pkt)
+
+def parseResponse():
+    atype = ord(ser.read(1))
+    alen = ord(ser.read(1))
+    payload=[]
+    for i in range(alen):
+        payload.append(ord(ser.read(1)))
+    if atype == 0x50:
+        print("Got ACK from {:x}{:x}".format(payload[0],payload[1]))
+    if atype == 0x51:
+        print("Got NACK from {:x}{:x}".format(payload[0],payload[1]))
+    if atype == 0x52:
+        print("Data from {:x}{:x}:{}".format(payload[0],payload[1],payload[2:]))
+    
+
+try: 
+    ser = serial.Serial(sys.argv[1],baudrate=115200)
+except:
+    print("Usage: {} <port>").format(sys.argv[0])
+
+
+while True:
+    sendpkt(0x0010,[0xDE,0xAD])
+    if (ser.inWaiting() >= 2):
+        parseResponse()
+    time.sleep(1)
